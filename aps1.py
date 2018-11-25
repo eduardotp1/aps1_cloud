77152053bf7301269f175a3019aad5863923a8c0
Ubuntu <ubuntu@ip-172-31-93-58.ec2.internal>
2018-11-25 22:57:27 +0000

    corrigindo erro endpoint

--- aps1.py
+++ aps1.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 dic={}
 count=0
 class Tarefas:
-    def __init__(self,id,nome):
+    def __init__(self,id,task):
         self.id = id
         self.task = task
 
@@ -17,11 +17,11 @@ def Tarefa():
     global count
     if request.method == 'POST':
         data = json.loads(request.data)
-        dict[count] = Tarefas(count,data['task'])
+        dic[count] = Tarefas(count,data['task'])
         count += 1
         return json.dumps({'status': 200}), 200
     else:
-        return json.dumps(dic)
+        return json.dumps([task.__dict__ for task in list(dic.values())])
 
 @app.route('/Tarefa/<id>', methods=['GET', 'PUT', 'DELETE'])
 def tarefa_id(id):
@@ -33,20 +33,20 @@ def tarefa_id(id):
         data = json.loads(request.data)
         if id in dic:
             dic[id] = Tarefas(id, data['task'])
-            return Response(status=200)
+            return json.dumps({'status': 200}), 200
         else:
-            return Response(status=404)
+            return json.dumps({'status': 204}), 204
     elif request.method == 'DELETE':
         if id in dic:
             del dic[id]
-            return Response(status=200)
+            return json.dumps({'status': 200}), 200
         else:
-            return Response(status=204)
+            return json.dumps({'status': 204}), 204
     else:
-        return json.dumps(Tarefas[id])
+        return json.dumps(dic[id].__dict__)
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    return Response(status=200)
+    return json.dumps({'status': 200}), 200
 
 app.run(host='0.0.0.0', port=5000)
